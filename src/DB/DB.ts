ce85f047f127ab6bdeda48fd951aa43c3490b5c8
Leytek <leytek@yandex.ru>
2022-07-15 16:54:11 +0300

    feat: add DB

--- src/DB/DB.ts
+++ src/DB/DB.ts
@@ -1,5 +1,27 @@
 export default class DB {
-  constructor() {
+  private data: Map<string, Map<string, unknown>>;
 
+  constructor() {
+    this.data = new Map();
+  }
+
+  createTable(name: string): void {
+    this.data.set(name, new Map());
+  }
+
+  createRecord(table: string, id: string, record: unknown): void {
+    this.data.get(table).set(id, record);
+  }
+
+  getRecord(table: string, id: string): unknown {
+    return this.data.get(table).get(id);
+  }
+
+  updateRecord(table: string, id: string, record: unknown): void {
+    this.data.get(table).set(id, record);
+  }
+
+  removeRecord(table: string, id: string): void {
+    this.data.get(table).delete(id);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,22 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlbumModule } from './endPoints/albums/albums.module';
-import { ArtistModule } from './endPoints/artists/artists.module';
-import { TrackModule } from './endPoints/tracks/tracks.module';
+import { AlbumsModule } from './endPoints/albums/albums.module';
+import { ArtistsModule } from './endPoints/artists/artists.module';
 import { FavoritesModule } from './endPoints/favorites/favorites.module';
-import { UserModule } from './endPoints/users/users.module';
+import { TracksModule } from './endPoints/tracks/tracks.module';
+import { UsersModule } from './endPoints/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    UsersModule,
-    ArtistsModule,
-    TracksModule,
     AlbumsModule,
+    ArtistsModule,
     FavoritesModule,
+    TracksModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
