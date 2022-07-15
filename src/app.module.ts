23e5347036711e6404249d9f3c71327aaff5e9f5
Leytek <leytek@yandex.ru>
2022-07-15 15:08:35 +0300

    feat: add base project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './endPoints/albums/albums.module';
+import { ArtistModule } from './endPoints/artists/artists.module';
+import { TrackModule } from './endPoints/tracks/tracks.module';
+import { FavoritesModule } from './endPoints/favorites/favorites.module';
+import { UserModule } from './endPoints/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
