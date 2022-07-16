7e90f75756745b6aa888447b12fc754c5ef46b9a
Leytek <leytek@yandex.ru>
2022-07-16 04:45:54 +0300

    feat: add validationPipe, swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  const docFile = await readFile('./doc/api.yaml', 'utf-8');
+  const document = parse(docFile);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
